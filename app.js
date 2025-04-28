05bf383b7505a8d02d5c83ea7e30b17f3aae39db
pierosalmena <piero.salmena@gmail.com>
2025-04-28 23:20:43 +0200

    Added REST operations for testing

--- app.js
+++ app.js
@@ -94,5 +94,24 @@ app.get("/", (req, res) => {
     });
   });
 
+  app.get('/', (req, res) => {
+    return res.send('Received a GET HTTP method');
+  });
+  
+  app.post('/', (req, res) => {
+    return res.send('Received a POST HTTP method');
+  });
+  
+  app.put('/', (req, res) => {
+    return res.send('Received a PUT HTTP method');
+  });
+  
+  app.delete('/', (req, res) => {
+    return res.send('Received a DELETE HTTP method');
+  });
+  
+  app.listen(process.env.PORT, () =>
+    console.log(`Example app listening on port ${process.env.PORT}!`),
+  );
 
 app.listen(3000, () => console.log("app listening on port 3000"))
