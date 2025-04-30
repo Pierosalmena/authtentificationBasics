8bab7ae58bda782bad9b4c7d77c7cdfd9b5bfcb1
pierosalmena <piero.salmena@gmail.com>
2025-04-30 02:18:16 +0200

    Added post and unique numbers

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs')
+const { v4: uuidv4 } = require('uuid');
+
+
 // const {signupRouter} = require("./signupRouter");
 
 const pool = new Pool ({
@@ -54,6 +57,8 @@ passport.use(
     next();
   });
 
+  app.use(express.json());
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -94,21 +99,65 @@ app.get("/", (req, res) => {
     });
   });
 
+let users = {
+    1: {
+      id: '1',
+      username: 'Robin Wieruch',
+    },
+    2: {
+      id: '2',
+      username: 'Dave Davids',
+    },
+  };
+  
+  let messages = {
+    1: {
+      id: '1',
+      text: 'Hello World',
+      userId: '1',
+    },
+    2: {
+      id: '2',
+      text: 'By World',
+      userId: '2',
+    },
+  };
+
   app.get('/users', (req, res) => {
-    res.send('GET HTTP method on user resource');
+    res.send(Object.values(users));
   });
+
+  app.get('/users/:userId', (req, res) => {
+    res.send(users[req.params.userId]);
+  })
+
+  app.get('/messages', (req,res) => {
+    res.send(Object.values(messages));
+  })
+
+  app.get('/messages/:messageId', (req, res) => {
+    return res.send(messages[req.params.messageId]);
+  });
+
+  app.post('/messages', (req, res) => {
+    const id = uuidv4();
+    const message = {id, text: req.body.text,}
+
+    messages[id] = message;
+    return res.send(message)
+  })
   
-  app.post('/users', (req, res) => {
-     res.send('POST HTTP method on user resource');
-  });
+//   app.post('/users', (req, res) => {
+//      res.send('POST HTTP method on user resource');
+//   });
   
-  app.put('/users:userId', (req, res) => {
-     res.send(`PUT HTTP method on user/${req.params.userId} resource`);
-  });
+//   app.put('/users/:userId', (req, res) => {
+//      res.send(`PUT HTTP method on user/${req.params.userId} resource`);
+//   });
   
-  app.delete('/users:userId', (req, res) => {
-     res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
-  });
+//   app.delete('/users/:userId', (req, res) => {
+//      res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
+//   });
   
 
 
